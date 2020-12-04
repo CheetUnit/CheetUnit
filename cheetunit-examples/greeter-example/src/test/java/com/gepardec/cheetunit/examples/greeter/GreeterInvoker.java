2c0b910c1e27d4c6031fe55665e54cf4604f64ff
Christian Salmhofer <christian.salmhofer@gepardec.com>
2020-12-04 11:45:00 +0100

    #15 Use Target-Folder as destinationPath for Plugin

--- cheetunit-examples/greeter-example/src/test/java/com/gepardec/cheetunit/examples/greeter/GreeterInvoker.java
+++ cheetunit-examples/greeter-example/src/test/java/com/gepardec/cheetunit/examples/greeter/GreeterInvoker.java
@@ -6,15 +6,13 @@
 package com.gepardec.cheetunit.examples.greeter;
 
 import com.gepardec.cheetunit.examples.greeter.service.GreeterServiceBaseInvoker;
-import com.gepardec.cheetunit.test.BaseServiceInvoker;
 import com.gepardec.cheetunit.test.CheetUnitConfig;
-import com.gepardec.cheetunit.test.CheetUnitConfigProvider;
 
 public class GreeterInvoker extends GreeterServiceBaseInvoker {
 
-  @Override
-  public CheetUnitConfig getConfig() {
-    // TODO use properties instead of override of getConfig
-    return new CheetUnitConfig("http", "localhost", "8080", "greeter/rest/cheetunit-insider", getAdditionalClasses());
-  }
+    @Override
+    public CheetUnitConfig getConfig() {
+        // TODO use properties instead of override of getConfig
+        return new CheetUnitConfig("http", "localhost", "8080", "greeter/rest/cheetunit-insider", getAdditionalClasses());
+    }
 }
--- cheetunit-invoker-generator-maven-plugin/src/main/java/com/gepardec/cheetunit/invokergenerator/helpers/BaseInvokerFileCreator.java
+++ cheetunit-invoker-generator-maven-plugin/src/main/java/com/gepardec/cheetunit/invokergenerator/helpers/BaseInvokerFileCreator.java
@@ -16,84 +16,80 @@ import static com.gepardec.cheetunit.invokergenerator.helpers.ClassUtils.extract
 import static com.gepardec.cheetunit.invokergenerator.helpers.ClassUtils.extractPackageName;
 
 public abstract class BaseInvokerFileCreator {
-  public static void createBaseInvokers(String destinationFolder, List<Class<?>> classes) throws IOException {
-    deleteAndCreateBaseDirectory(destinationFolder);
+    public static void createBaseInvokers(String destinationFolder, List<Class<?>> classes) throws IOException {
+        deleteAndCreateBaseDirectory(destinationFolder);
 
-    for (Class<?> aClass : classes) {
-      createBaseInvoker(destinationFolder, aClass);
-    }
-  }
-
-  private static void createBaseInvoker(String destinationFolder, Class<?> aClass) throws IOException {
-    String packageDirectoryPath = createPackageFolderAndGetPath(destinationFolder, aClass);
-
-    File baseInvokerFile = createBaseInvokerJavaFile(packageDirectoryPath, aClass);
-
-    writeContentToFile(baseInvokerFile, aClass);
-  }
-
-  private static void writeContentToFile(File baseInvokerFile, Class<?> aClass) throws IOException {
-      FileWriter contentWriter = new FileWriter(baseInvokerFile);
-      BaseInvokerContentCreator.getBaseInvokerContent(aClass);
-      contentWriter.write(BaseInvokerContentCreator.getBaseInvokerContent(aClass));
-      contentWriter.close();
-  }
-
-  private static File createBaseInvokerJavaFile(String packageDirectoryPath, Class<?> aClass) throws IOException {
-    File invokerJavaFile = new File(packageDirectoryPath + File.separator + extractClassName(aClass) + "BaseInvoker.java");
-
-    invokerJavaFile.createNewFile();
-
-    return invokerJavaFile;
-  }
-
-  private static String createPackageFolderAndGetPath(String destinationFolder, Class<?> aClass) {
-    String packagePath = getPackagePath(destinationFolder, aClass);
-    File packageDir = new File(packagePath);
-
-    if (!packageDir.exists()) {
-      packageDir.mkdirs();
+        for (Class<?> aClass : classes) {
+            createBaseInvoker(destinationFolder, aClass);
+        }
     }
 
-    return packagePath;
-  }
+    private static void createBaseInvoker(String destinationFolder, Class<?> aClass) throws IOException {
+        String packageDirectoryPath = createPackageFolderAndGetPath(destinationFolder, aClass);
 
-  private static String getPackagePath(String destinationFolder, Class<?> aClass) {
-    String packageFolderStructure = extractPackageName(aClass).replace(".", File.separator);
+        File baseInvokerFile = createBaseInvokerJavaFile(packageDirectoryPath, aClass);
 
-    return getDestinationPath(destinationFolder) + File.separator + packageFolderStructure;
-  }
-
-  private static void deleteAndCreateBaseDirectory(String destinationFolder) {
-    File dir = new File(getDestinationPath(destinationFolder));
-
-    if (dir.exists()) {
-      deleteDirectory(new File(destinationFolder));
+        writeContentToFile(baseInvokerFile, aClass);
     }
-    dir.mkdirs();
-  }
 
-
-
-  private static String getDestinationPath(String destinationFolder) {
-    return destinationFolder
-        + File.separator + "test"
-        + File.separator + "java";
-  }
-
-  private static String getBaseDirectoryPath(MavenProject project) {
-    return project.getBasedir().getAbsolutePath();
-  }
-
-  private static void deleteDirectory(File directoryToBeDeleted) {
-    File[] allContents = directoryToBeDeleted.listFiles();
-    if (allContents != null) {
-      for (File file : allContents) {
-        deleteDirectory(file);
-      }
+    private static void writeContentToFile(File baseInvokerFile, Class<?> aClass) throws IOException {
+        FileWriter contentWriter = new FileWriter(baseInvokerFile);
+        BaseInvokerContentCreator.getBaseInvokerContent(aClass);
+        contentWriter.write(BaseInvokerContentCreator.getBaseInvokerContent(aClass));
+        contentWriter.close();
     }
-    directoryToBeDeleted.delete();
-  }
+
+    private static File createBaseInvokerJavaFile(String packageDirectoryPath, Class<?> aClass) throws IOException {
+        File invokerJavaFile = new File(packageDirectoryPath + File.separator + extractClassName(aClass) + "BaseInvoker.java");
+
+        invokerJavaFile.createNewFile();
+
+        return invokerJavaFile;
+    }
+
+    private static String createPackageFolderAndGetPath(String destinationFolder, Class<?> aClass) {
+        String packagePath = getPackagePath(destinationFolder, aClass);
+        File packageDir = new File(packagePath);
+
+        if (!packageDir.exists()) {
+            packageDir.mkdirs();
+        }
+
+        return packagePath;
+    }
+
+    private static String getPackagePath(String destinationFolder, Class<?> aClass) {
+        String packageFolderStructure = extractPackageName(aClass).replace(".", File.separator);
+
+        return getDestinationPath(destinationFolder) + File.separator + packageFolderStructure;
+    }
+
+    private static void deleteAndCreateBaseDirectory(String destinationFolder) {
+        File dir = new File(getDestinationPath(destinationFolder));
+
+        if (dir.exists()) {
+            deleteDirectory(new File(destinationFolder));
+        }
+        dir.mkdirs();
+    }
+
+    private static String getDestinationPath(String destinationFolder) {
+        return destinationFolder;
+    }
+
+    private static String getBaseDirectoryPath(MavenProject project) {
+        return project.getBasedir().getAbsolutePath();
+    }
+
+    private static void deleteDirectory(File directoryToBeDeleted) {
+        File[] allContents = directoryToBeDeleted.listFiles();
+        if (allContents != null) {
+            for (File file : allContents) {
+                deleteDirectory(file);
+            }
+        }
+        directoryToBeDeleted.delete();
+    }
 
 
 }
