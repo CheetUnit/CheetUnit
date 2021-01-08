f1f169885a9f028f9eb41ea92638a55b79c1f99c
Christian Salmhofer <christian.salmhofer@gepardec.com>
2021-01-08 09:33:53 +0100

    #8 Extend documentation of CheetUnitNoTransactionRequired

--- cheetunit-core/src/main/java/com/gepardec/cheetunit/core/annotations/CheetUnitNoTransactionRequired.java
+++ cheetunit-core/src/main/java/com/gepardec/cheetunit/core/annotations/CheetUnitNoTransactionRequired.java
@@ -11,7 +11,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * CheetUnit will not open a new transaction when this annotation is used on invoker-method
+ * CheetUnit will not open a new transaction when this annotation is used on invoker-method or invoker-class.
  *
  * Use this annotation if you manage your transactions manually. You don't need to use it, if your transactions are
  * managed by container.
